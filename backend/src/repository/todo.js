0b10791eb58c7788a16ad57a1f5f12fc15756e4e
Robell <robellasfaw@gmail.com>
2025-07-08 06:51:15 +0100

    Add method for createTodo

--- backend/src/repository/todo.js
+++ backend/src/repository/todo.js
@@ -1,12 +1,16 @@
-
 let todoList = {
   todos: [
     {
-      "task": "This is a todo example"
-    }
-  ]
+      task: "This is a todo example",
+    },
+  ],
 };
 
 module.exports = {
-  getTodos: () => Promise.resolve(todoList)
+  getTodos: () => Promise.resolve(todoList),
+  //Created a createTodo method to push new tasks to the array of task objects
+  createTodo: () => {
+    todoList.todos.push({ task: newTask });
+    return Promise.resolve(todoList);
+  },
 };
