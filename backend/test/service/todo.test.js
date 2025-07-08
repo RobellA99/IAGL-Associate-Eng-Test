9b7d36acdaeec322e8f598d779d79581f7efa180
Robell <robellasfaw@gmail.com>
2025-07-08 08:58:54 +0100

    Add service layer test

--- backend/test/service/todo.test.js
+++ backend/test/service/todo.test.js
@@ -1,19 +1,33 @@
-
-
-describe('TODO Service', () => {
-  it('should be able to get todos from repository', async () => {
+describe("TODO Service", () => {
+  it("should be able to get todos from repository", async () => {
     const expected = {
       todos: [
         {
-          task: "This is a task to be done"
-        }
-      ]
+          task: "This is a task to be done",
+        },
+      ],
     };
     const todoRepository = {
-      getTodos: async () => Promise.resolve(expected)
+      getTodos: async () => Promise.resolve(expected),
     };
 
-    const todoService = require('../../src/service/todo')(todoRepository);
+    it("should create a new todo through the repository", async () => {
+      const task = "Write service tests";
+
+      const mockRepository = {
+        createTodo: jest.fn(async (task) => ({
+          todos: [{ task }],
+        })),
+      };
+
+      const todoService = require("../../src/service/todo")(mockRepository);
+      const result = await todoService.createTodo(task);
+
+      expect(mockRepository.createTodo).toHaveBeenCalledWith(task);
+      expect(result.todos).toContainEqual({ task });
+    });
+
+    const todoService = require("../../src/service/todo")(todoRepository);
     const actual = await todoService.getTodos();
     expect(actual).toEqual(expected);
   });
