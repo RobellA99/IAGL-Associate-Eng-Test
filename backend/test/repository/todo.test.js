1583692cac6fd489c38e3c09881bb92312bf1dfd
Robell <robellasfaw@gmail.com>
2025-07-08 08:57:56 +0100

    Add repo layer test

--- backend/test/repository/todo.test.js
+++ backend/test/repository/todo.test.js
@@ -1,13 +1,23 @@
+const repository = require("../../src/repository/todo");
 
-const repository = require('../../src/repository/todo');
-
-describe('TODO repository', () => {
-  it('should return the todo list', async () => {
+describe("TODO repository", () => {
+  it("should return the todo list", async () => {
     const expected = {
-      todos: [{
-        "task": "This is a todo example"
-      }]
+      todos: [
+        {
+          task: "This is a todo example",
+        },
+      ],
     };
+
+    it("should add a new todo to the list", async () => {
+      const newTask = "Learn testing";
+      await repository.createTodo(newTask);
+
+      const updatedTodos = await repository.getTodos();
+      expect(updatedTodos.todos).toContainEqual({ task: newTask });
+    });
+
     const actual = await repository.getTodos();
     expect(actual).toEqual(expected);
   });
