e4f4915b730fe6183325d965401d277f641e95ef
Robell <robellasfaw@gmail.com>
2025-07-08 07:42:00 +0100

    Add form + styling

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,34 +1,114 @@
-import React, {Component} from "react";
+// import React, { Component} from "react";
+// import Todo from "./Todo";
+// import { fetchTodos } from "../actions";
+// import { connect } from "react-redux";
+
+// class TodoList extends Component {
+//   state = {
+//     newTask: "",
+//   };
+
+//   componentDidMount() {
+//     this.props.fetchTodos();
+//   }
+
+//   handleChange = (e) => {
+//     this.setState({ newTask: e.target.value });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (this.state.newTask.trim()) {
+//       this.props.postTodo(this.state.newTask.trim());
+//       this.setState({ newTask: "" });
+//     }
+//   };
+
+//   render() {
+//     const { todos } = this.props.data;
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit} className="todo-form">
+//           <input
+//             type="text"
+//             value={this.state.newTask}
+//             onChange={this.handleChange}
+//             placeholder="Enter new task"
+//             className="todo-input"
+//           />
+//           <button type="submit" className="todo-button">
+//             Submit task
+//           </button>
+//         </form>
+//         <ul className="todo-list">
+//           {todos && todos.length
+//             ? todos.map((todo, index) => {
+//                 return <Todo key={`todo-${index}`} todo={todo.task} />;
+//               })
+//             : "No todos, yay!"}
+//         </ul>
+//       </>
+//     );
+//   }
+// }
+
+// const mapStateToProps = ({ data = {}, isLoadingData = false }) => ({
+//   data,
+//   isLoadingData,
+// });
+// export default connect(mapStateToProps, {
+//   fetchTodos,
+// })(TodoList);
+
+import React, { useState, useEffect } from "react";
 import Todo from "./Todo";
-import {fetchTodos} from "../actions";
-import {connect} from "react-redux";
+import { fetchTodos } from "../actions";
+import { useDispatch, useSelector } from "react-redux";
 
-class TodoList extends Component {
-  state = {};
+const TodoList = ({ postTodo }) => {
+  const [newTask, setNewTask] = useState("");
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.data.todos);
 
-  componentDidMount() {
-    this.props.fetchTodos();
-  }
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
 
-  render() {
-    const {todos} = this.props.data;
-    return (<ul className="todo-list">
-      {todos && todos.length
-        ? todos.map((todo, index) => {
-          return <Todo key={`todo-${index}`} todo={todo.task}/>;
-        })
-        : "No todos, yay!"}
-    </ul>);
-  }
-}
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
 
-const mapStateToProps = ({data = {}, isLoadingData = false}) => ({
-  data,
-  isLoadingData
-});
-export default connect(
-  mapStateToProps,
-  {
-    fetchTodos
-  }
-)(TodoList);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask.trim()) {
+      postTodo(newTask.trim());
+      setNewTask("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="todo-form">
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleChange}
+          placeholder="Enter new task"
+          className="todo-input"
+        />
+        <button type="submit" className="todo-button">
+          Submit task
+        </button>
+      </form>
+      <ul className="todo-list">
+        {todos && todos.length
+          ? todos.map((todo, index) => (
+              <Todo key={`todo-${index}`} todo={todo.task} />
+            ))
+          : "No todos, yay!"}
+      </ul>
+    </>
+  );
+};
+
+export default TodoList;
