5dd431a0818a8dc013822c2abb4f1a0714726fc4
Robell <robellasfaw@gmail.com>
2025-07-08 06:57:54 +0100

    Add POST axios call

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,16 @@ import axios from "axios";
 import { FETCH_TODOS } from "./types";
 
 export function fetchTodos() {
-  return function(dispatch) {
+  return function (dispatch) {
     return axios.get("http://localhost:9091/api/todo").then(({ data }) => {
+      dispatch(setTodos(data));
+    });
+  };
+}
+
+export function postTodo(task) {
+  return function (dispatch) {
+    return axios.post("http://localhost:9091/api/todo", { task }).then(() => {
       dispatch(setTodos(data));
     });
   };
@@ -12,6 +20,6 @@ export function fetchTodos() {
 function setTodos(data) {
   return {
     type: FETCH_TODOS,
-    payload: data
+    payload: data,
   };
 }
